1d313d1dc447d0db98aaf48b98665cb72ca24098
RAJATS SHARMA <108745640+rajat-sha7@users.noreply.github.com>
2023-12-03 18:02:43 +0530

    ok

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 import "./Login.css"
 import { useNavigate } from 'react-router-dom'
 import HeaderLogin from '../../Headers/HeaderLogin'
